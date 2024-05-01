3f1e2e36911ed67238b7577377bb62fc7f5c0907
NoorSbeih <121386818+NoorSbeih@users.noreply.github.com>
2024-05-02 01:19:02 +0300

    some fixes

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:fit_scoop/testworkouts.dart';
 import 'package:flutter/material.dart';
 
 import 'Views/Screens/home_page_screen.dart';
 import 'Views/Screens/register_screen.dart';
+import 'Controllers/workout_controller.dart';
 
 void main() async {
   print("Ba70000");
@@ -20,6 +22,7 @@ void main() async {
   runApp(const MyApp());
 }
 
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -33,7 +36,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
         useMaterial3: true,
       ),
-      home: MyHomePage(title: '',),
+      home: const MyHomePage(title: '',),
     /*  routes: {
        // '/sign_up_view': (context) => SignUpPage(),
       //  '/home_view': (context) => const HomeView(),
