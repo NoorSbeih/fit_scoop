302b63143e18a4b08b832b5b137035ee07a076dd
NoorSbeih <121386818+NoorSbeih@users.noreply.github.com>
2024-05-02 01:19:51 +0300

    Add workoutschedule attribute

--- lib/Models/body_metrics_model.dart
+++ lib/Models/body_metrics_model.dart
@@ -1,53 +1,58 @@
 
+import 'package:fit_scoop/Models/workout_model.dart';
+
 class BodyMetrics {
- // final String id;
-  final String user_id; // User ID associated with these metrics
+  final String userId; // User ID associated with these metrics
   final double height;
-  final double  weight;
+  final double weight;
   final DateTime birthDate;
   final String gender;
   final double bodyFat;
-  final List <String> fitnessGoal;
-  final String gymOrHome;
+  final List<String> fitnessGoal;
+  final String gymType;
+  final List<List<String>> workoutSchedule; // List of workouts for each day
 
   BodyMetrics({
-  //  required this.id,
-    required this.user_id,
+    required this.userId,
     required this.height,
     required this.weight,
     required this.birthDate,
     required this.gender,
     required this.bodyFat,
     required this.fitnessGoal,
-    required this.gymOrHome,
-  });
+    required this.gymType,
+    List<List<String>>? workoutSchedule, // Optional parameter for workout schedule
+  }) : this.workoutSchedule = workoutSchedule ?? List.generate(7, (_) => []);
 
   // Convert BodyMetricsModel to a map (for Firestore)
   Map<String, dynamic> toMap() {
     return {
-      'user_id':user_id,
+      'userId': userId,
       'height': height,
       'weight': weight,
       'birthDate': birthDate,
       'gender': gender,
-      'bodyFat':bodyFat,
+      'bodyFat': bodyFat,
       'fitnessGoal': fitnessGoal,
-      'gymOrHome': gymOrHome,
+      'gymType': gymType,
+      'workoutSchedule': workoutSchedule,
     };
   }
 
   factory BodyMetrics.fromMap(Map<String, dynamic> map) {
     return BodyMetrics(
+      userId: map['userId'],
       height: map['height'],
       weight: map['weight'],
       birthDate: map['birthDate'],
       gender: map['gender'],
       bodyFat: map['bodyFat'],
-      fitnessGoal: map['fitnessGoal'],
-      gymOrHome: map['gymOrHome'],
-   //   id: map['id'],
-     user_id:map['user_id']
+      fitnessGoal: List<String>.from(map['fitnessGoal'] ?? []),
+      gymType: map['gymType'],
+      workoutSchedule: List<List<String>>.from(map['workoutSchedule'] ?? List.generate(7, (_) => [])),
     );
   }
 }
+
+
 
