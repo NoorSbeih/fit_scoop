4955a173b490a3b3634b0f378d68840fb0d654c6
Eliana <elianaellati2002@gmail.com>
2024-06-22 22:37:11 +0300

    Register

---
+++ lib/Views/Screens/GenerateWorkout/diastolic_systolic_screen.dart
@@ -0,0 +1,223 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:numberpicker/numberpicker.dart';
+import 'package:fit_scoop/Views/Widgets/custom_widget.dart';
+
+class DiastolicSystolicScreen extends StatefulWidget {
+  const DiastolicSystolicScreen({Key? key}) : super(key: key);
+
+  @override
+  State<DiastolicSystolicScreen> createState() => _DiastolicSystolicScreen();
+}
+
+class _DiastolicSystolicScreen extends State<DiastolicSystolicScreen> {
+  late TextEditingController selectedDiastolic = TextEditingController();
+  late TextEditingController selectedSystolic = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    // Initialize text controllers or perform other setup if needed
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: const Color(0xFF2C2A2A),
+        appBar: AppBar(
+          backgroundColor: const Color(0xFF2C2A2A),
+          iconTheme: const IconThemeData(
+            color: Color(0xFF0dbab4), // Change the drawer icon color here
+          ),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 10, left: 16, bottom: 10),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: custom_widget.startTextWidget("Diastolic and Systolic"),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 60.0, left: 16, right: 16),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: custom_widget.customTextWidget(
+                  "Entering your systolic and diastolic blood pressure is essential for generating the best workouts and ensuring they are safe and effective for your heart health.",
+                  15,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: RichText(
+                  textAlign: TextAlign.left,
+                  text: const TextSpan(
+                    text: "Diastolic",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0,
+                    ),
+                    children: [
+                      TextSpan(
+                        text: ' *',
+                        style: TextStyle(
+                          color: Colors.red,
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(15),
+              child: _buildDiastolicPicker(context),
+            ),
+            SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: RichText(
+                  textAlign: TextAlign.left,
+                  text: const TextSpan(
+                    text: "Systolic",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0,
+                    ),
+                    children: [
+                      TextSpan(
+                        text: ' *',
+                        style: TextStyle(
+                          color: Colors.red,
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(15),
+              child: _buildSystolicPicker(context),
+            ),
+            SizedBox(height: 50),
+            ElevatedButton(
+              onPressed: () {
+                //  AddWorkoutForADay()
+               /* Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) =>
+                        selectDayForLibraryy(workout :widget.workout),
+                  ),
+                );*/
+
+              },
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(
+                    const Color(0xFF0dbab4)), // Change color to blue
+                fixedSize:
+                MaterialStateProperty.all<Size>(   const Size(190, 50)),
+                shape: MaterialStateProperty.resolveWith<OutlinedBorder>(
+                      (Set<MaterialState> states) {
+                    return RoundedRectangleBorder(
+                      borderRadius:
+                      BorderRadius.circular(5.0), // Border radius
+                    );
+                  },
+                ),
+              ),
+              child: const Text(
+                'NEXT',
+                style: TextStyle(fontSize: 22, color: Colors.white),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildDiastolicPicker(BuildContext context) {
+    return custom_widget.textFormFieldWidget(
+      "Select Diastolic",
+      selectedDiastolic,
+      context,
+      "MM HG",
+      _buildNumberPickerInDia,
+    );
+  }
+
+  Widget _buildSystolicPicker(BuildContext context) {
+    return custom_widget.textFormFieldWidget(
+      "Select Systolic",
+      selectedSystolic,
+      context,
+      "MM HG",
+      _buildNumberPickerInSys,
+    );
+  }
+
+  Widget _buildNumberPickerInDia() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        _buildNumberPicker(80, "mm Hg", 80, 120, 80, selectedDiastolic),
+      ],
+    );
+  }
+
+  Widget _buildNumberPickerInSys() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        _buildNumberPicker(120, "mm Hg", 120, 180, 80, selectedSystolic),
+      ],
+    );
+  }
+
+  Widget _buildNumberPicker(int x, String text, int minValue, int maxValue, double itemWidth, TextEditingController controller) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        StatefulBuilder(
+          builder: (context, setState) {
+            return NumberPicker(
+              value: x,
+              minValue: minValue,
+              maxValue: maxValue,
+              itemHeight: 50,
+              itemWidth: itemWidth,
+              onChanged: (value) {
+                setState(() {
+                  x = value;
+                  controller.text = value.toString(); // Update the text in the controller
+                });
+              },
+              selectedTextStyle: const TextStyle(fontSize: 16, color: Colors.white),
+              axis: Axis.vertical,
+              decoration: const BoxDecoration(
+                border: Border(
+                  top: BorderSide(color: Colors.white, width: 3),
+                  bottom: BorderSide(color: Colors.white, width: 3),
+                ),
+              ),
+            );
+          },
+        ),
+        custom_widget.customTextWidget(text, 15),
+      ],
+    );
+  }
+}
+
--- lib/Views/Widgets/drawer_widget.dart
+++ lib/Views/Widgets/drawer_widget.dart
@@ -10,6 +10,7 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../Controllers/authentication_controller.dart';
+import '../Screens/GenerateWorkout/diastolic_systolic_screen.dart';
 import '../Screens/Update/gymType.dart';
 import '../Screens/Update/maleGoalsUpdate.dart';
 import '../Screens/Update/weight_HeightUpdate.dart';
@@ -41,22 +42,61 @@ class _CustomDrawerState extends State<CustomDrawer> {
         UserSingleton usersingleton = UserSingleton.getInstance();
         User_model user = usersingleton.getUser();
         equipments=user.savedEquipmentIds.length.toString();
-
         return Drawer(
           backgroundColor: Color(0xFF2C2A2A),
           child: ListView(
             padding: EdgeInsets.zero,
             children: <Widget>[
-               const DrawerHeader(
-                child: Text(
-                  'FitScoop',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 24,
-                      fontFamily: 'BebasNeue')
-                  ),
+              DrawerHeader(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'FitScoop',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                        fontFamily: 'BebasNeue',
+                      ),
+                    ),
+          Center(
+            child:Container(
+              margin: EdgeInsets.symmetric(vertical: 20),
+              width: 250, // Set the width of the Container
+              height: 50, // Set the height of the Container
+              decoration: BoxDecoration(
+                color: Color(0xFF0dbab4),
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(color: Colors.red, width: 0),
+              ),
+              child:Center(
+                child:ListTile(
+                      leading: Icon(Icons.fitness_center, color: Colors.white),
+                      title: const Text(
+                        'GENERATE WORKOUT',
+                         style: TextStyle(
+                           color: Colors.white,
+                          fontSize: 20,
+                          fontFamily: 'BebasNeue',
+                        ),
+                      ),
+                      onTap: () async {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) =>  DiastolicSystolicScreen()
+                            ),
+                          );
 
+
+                      },
+                    ),
+              ),
+            ),
+          )
+                  ],
                 ),
+              ),
               ListTile(
                 title: const Text(
                   'GYM EQUIPMENT',
